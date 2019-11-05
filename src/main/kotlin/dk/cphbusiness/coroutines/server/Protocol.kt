a21f7bb3ae9e017af8b8593f392a6a8913efeca1
eguahlak <anders@kalhauge.dk>
2019-11-05 11:12:03 +0100

    server template

--- src/main/kotlin/dk/cphbusiness/coroutines/server/Protocol.kt
+++ src/main/kotlin/dk/cphbusiness/coroutines/server/Protocol.kt
@@ -1,6 +1,9 @@
 package dk.cphbusiness.coroutines.server
 
+import java.io.ByteArrayOutputStream
 import java.io.InputStream
+import java.io.OutputStream
+import java.lang.StringBuilder
 
 enum class Method { GET, PUT, POST, DELETE }
 
@@ -15,3 +18,33 @@ class Request(input: InputStream) {
     method = Method.valueOf(parts[0])
     }
   }
+
+class Response(private val output: OutputStream) {
+  val body = StringBuilder()
+
+  fun append(text: String) {
+    body.append(text)
+    }
+
+  fun send() {
+    val head = """
+        HTTP/1.1 200 OK
+        Content-Type: text/html; charset=UTF-8
+        Content-Length: ${body.length}
+        Connection: close
+        
+        """.trimIndent()
+    val writer = output.bufferedWriter()
+    writer.append(head)
+    writer.newLine()
+    writer.append(body)
+    writer.close()
+    }
+
+  }
+/*
+fun main() {
+  val output = ByteArrayOutputStream(1024)
+  val writer = output.bufferedWriter()
+  }
+ */
--- src/test/kotlin/dk/cphbusiness/coroutines/server/ProtocolTest.kt
+++ src/test/kotlin/dk/cphbusiness/coroutines/server/ProtocolTest.kt
@@ -2,6 +2,7 @@ package dk.cphbusiness.coroutines.server
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.io.ByteArrayOutputStream
 
 class ProtocolTest {
   val requestText = """
@@ -21,7 +22,27 @@ class ProtocolTest {
     val input = requestText.byteInputStream()
     val request = Request(input)
     assertEquals(Method.GET, request.method)
-   }
+    }
+
+  val responseText = """
+      HTTP/1.1 200 OK
+      Content-Type: text/html; charset=UTF-8
+      Content-Length: 18
+      Connection: close
+      
+      <p>Hello Kurt!</p>
+      """.trimIndent()
+
+  @Test
+  fun testSayHelloToKurt() {
+    val output = ByteArrayOutputStream(1024)
+    val response = Response(output)
+    response.append("<p>Hello")
+    response.append(" Kurt!</p>")
+    response.send()
+    println(output.toString())
+    assertEquals(responseText, output.toString())
+    }
 
   }
 
